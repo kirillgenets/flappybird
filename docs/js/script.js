ef79f86d050b0cf2bd1766f5af076284f5d9344c
Kirill Genets <kirill.genets2002@mail.ru>
2019-08-09 12:36:57 +0300

    prepared all files + started to write js

---
+++ docs/js/script.js
@@ -0,0 +1,20 @@
+const canvas = document.querySelector('#game');
+const ctx = canvas.getContext('2d');
+
+const bird = new Image();
+const background = new Image();
+const foreground = new Image();
+const pipeUp = new Image();
+const pipeBottom = new Image();
+
+bird.src = 'img/flappy_bird_bird.png';
+background.src = 'img/flappy_bird_bg.png';
+foreground.src = 'img/flappy_bird_fg.png';
+pipeUp.src = 'img/flappy_bird_pipeUp.png';
+pipeBottom.src = 'img/flappy_bird_pipeBottom.png';
+
+function grow() {
+
+	
+	
+}
